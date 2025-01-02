b8eca0c0ceb6a4d9eb9e39d0e1506e96bbe0f313
asiripr <asiripramodaya2000@gmail.com>
2025-01-02 18:30:12 +0530

    other components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Intro from './components/Intro';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Volunteering from './components/Vounteering';
 
 function App() {
   return (
@@ -19,65 +22,19 @@ function App() {
       <Projects />
 
       {/* Experience Section */}
-      <section id="experience" className="py-5">
-        <div className="container">
-          <h2 className="text-center">Experience</h2>
-          <p>
-            <strong>Society of Computer Sciences Member:</strong> Organized IT workshops and coding
-            challenges at Sabaragamuwa University of Sri Lanka.
-          </p>
-        </div>
-      </section>
-
+      <Experience />
 
       {/* Education Section */}
-      <section id="education" className="py-5">
-        <div className="container">
-          <h2 className="text-center">Education</h2>
-          <ul className="list-group">
-            <li className="list-group-item">
-              <strong>
-                <a href="https://www.sab.ac.lk/computing/" target="_blank">
-                  Sabaragamuwa University of Sri Lanka:
-                </a>
-              </strong>
-              Bachelor's degree in Computing and Information Systems (Jul 2022 - 2026)
-            </li>
-            <li className="list-group-item">
-              <strong>
-                <a href="https://stcmatara.lk/" target="_blank">
-                  St. Thomas' College, Matara:
-                </a>
-              </strong>
-              Physical Science Stream (2017 - 2020)
-            </li>
-          </ul>
-        </div>
-      </section>
+      <Education />
 
       {/* Volunteering Section */}
-      <section id="volunteering" className="py-5 bg-light">
-        <div className="container">
-          <h2 className="text-center">Volunteering</h2>
-          <ul className="list-group">
-            <li className="list-group-item">
-              <strong>Member, IEEE Student Branch of SUSL:</strong> Volunteered as IEEE Day 2024
-              ambassador for Sabaragamuwa University of Sri Lanka.
-            </li>
-            <li className="list-group-item">
-              <strong>Member, Society of Computer Sciences - SUSL:</strong> Active member since August
-              2022, contributing to the university's IT and computing community.
-            </li>
-          </ul>
-        </div>
-      </section>
-      
+      <Volunteering />
+
       {/* Skills Section */}
       <Skills />
 
       {/* Contact Section */}
       <Contact />
-
 
     </>
   );
---
+++ src/components/Education.jsx
@@ -0,0 +1,29 @@
+function Education() {
+    return (
+        <section id="education" className="py-5">
+            <div className="container">
+                <h2 className="text-center">Education</h2>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <strong>
+                            <a href="https://www.sab.ac.lk/computing/" target="_blank">
+                                Sabaragamuwa University of Sri Lanka:
+                            </a>
+                        </strong>
+                        Bachelor's degree in Computing and Information Systems (Jul 2022 - 2026)
+                    </li>
+                    <li className="list-group-item">
+                        <strong>
+                            <a href="https://stcmatara.lk/" target="_blank">
+                                St. Thomas' College, Matara:
+                            </a>
+                        </strong>
+                        Physical Science Stream (2017 - 2020)
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
+}
+
+export default Education;
